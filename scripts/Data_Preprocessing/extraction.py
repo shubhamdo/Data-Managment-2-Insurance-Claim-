2bafaf84022e54c28ba2784d1ca6d40892482bfc
shubh <shubhamshashikantshinde@outlook.com>
2020-11-10 02:15:05 +0100

    Data Extraction Class

--- scripts/Data_Preprocessing/extraction.py
+++ scripts/Data_Preprocessing/extraction.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from scripts.databaseconnection import MySqlConnect
 
-db_connection = MySqlConnect()
 
 class Extract:
 
@@ -31,4 +30,16 @@ class Extract:
     def policy_state_dim(self):
         policy_state_dim = pd.read_sql('SELECT * FROM policy_state_dim where scd_end is null', con=self.connection)
         return  policy_state_dim
+
+if __name__ == '__main__':
+    db = MySqlConnect()
+    db_connection = db.connect()
+    extract_obj = Extract(db_connection)
+    address_dim = extract_obj.address_dim()
+    customer_dim = extract_obj.customer_dim()
+    policy_csl_dim = extract_obj.policy_csl_dim()
+    incidents_dim = extract_obj.incidents_dim()
+    policy_fact = extract_obj.policy_fact()
+    policy_state_dim = extract_obj.policy_state_dim()
+
 
